1ca904cbc4d139fc86c22cc6893ba6a592c34c0f
Raykito <raydeur29@gmail.com>
2016-10-22 18:03:05 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#intra").click(function(){
+    $("#displayPerso").hide()
+    $("#displayIntra").fadeToggle(200)
+  })
+  $("#perso").click(function(){
+    $("#displayIntra").hide()
+    $("#displayPerso").fadeToggle(200)
+  })
+})
+
+function time() {
+  var date = new Date()
+  var hours = date.getHours()
+  if (hours < 10)
+    hours = "0" + hours
+  var mins = date.getMinutes()
+  if (mins < 10)
+    mins = "0" + mins
+  document.getElementById("displayTime").innerHTML = hours + " : " + mins
+
+  setTimeout(time, 1000)
+}
+
+time()
